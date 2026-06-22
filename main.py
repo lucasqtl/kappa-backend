f9357bbe65cf46ddc6b9d1719bbe445c23d8bfe7
Luiz Bomfim <leeshupativani@gmail.com>
2026-06-22 17:02:17 -0300

    main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from pathlib import Path
 
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.responses import JSONResponse
 from sqlalchemy import text
@@ -26,6 +27,14 @@ app = FastAPI(
     version="0.1.0",
     docs_url="/docs",
     redoc_url="/redoc",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix=settings.api_v1_prefix)
